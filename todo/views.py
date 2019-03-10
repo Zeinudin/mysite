877583377866ee21577f32ef94fc911bedf94959
Zeinudin <lirov_z@iuca.kg>
2019-03-10 16:57:03 +0600

    The End My first project

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+from .models import Zadacha
 # Create your views here.
 
 def post_list(request):
-	n = ['lirov_z', 'oleg', 'viktor', 'irina']
-	return render(request, 'todo/index.html', context={'names': n})
+	n = Zadacha.objects.all()
+	return render(request, 'todo/index.html', context={'Zadacha': n})
