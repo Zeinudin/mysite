78153004fc240ceca9759ddd2efa87d76c8ac986
Zeinudin <33855044+Zeinudin@users.noreply.github.com>
2019-04-08 17:17:40 +0600

    Delete views.py

--- todo/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from .models import Zadacha
-# Create your views here.
-
-def post_list(request):
-	n = Zadacha.objects.all()
-	return render(request, 'todo/index.html', context={'Zadacha': n})
